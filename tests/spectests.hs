27fe99c1dc41cf5d78eb3cae4bffc105cd929c4d
Francesco <f@mazzo.li>
2012-02-05 13:53:47 +0000

    Cabal install update tests

--- tests/spectests.hs
+++ tests/spectests.hs
@@ -1,6 +1,7 @@
 {-# Language OverloadedStrings #-}
 
 import Database.KyotoCabinet.DB.Hash
+import Database.KyotoCabinet.Operations
 import Data.ByteString.Char8 ()
 
 import Prelude hiding (iterate)
@@ -8,30 +9,32 @@ import Prelude hiding (iterate)
 test1 :: IO ()
 test1 = do
   -- Create the DB
-  db <- openPersistent Hash "/tmp/casket.kch" defaultLoggingOptions [] (Writer [Create] [])
+  db <- makeHash "/tmp/casket.kch" defaultLoggingOptions defaultHashOptions (Writer [Create] [])
 
   set db "foo" "hop"
   set db "bar" "step"
   set db "baz" "jump"
 
   get db "foo" >>= putStrLn . show
-  
+
   -- This will be done with cursors
-  let visitor = \k v -> putStr (show k) >> putStr ":" >> putStrLn (show v) >> return (Left NoOperation)
+  let visitor = \k v -> putStr (show k) >> putStr ":" >> putStrLn (show v) >>
+                        return (Left NoOperation)
   iterate db visitor False
 
   close db
 
 test2 :: IO ()
 test2 = do
-  db <- openPersistent Hash "/tmp/casket.kch" defaultLoggingOptions [] (Reader [])
+  db <- openHash "/tmp/casket.kch" defaultLoggingOptions (Reader [])
 
-  let vfull  = \k v -> putStr (show k) >> putStr ":" >> putStrLn (show v) >> return (Left NoOperation)
+  let vfull  = \k v -> putStr (show k) >> putStr ":" >> putStrLn (show v) >>
+                       return (Left NoOperation)
       vempty = \k   -> putStr (show k) >> putStrLn " is missing" >> return Nothing
 
   accept db "foo" vfull vempty False
   accept db "dummy" vfull vempty False
-  
+
   iterate db vfull False
 
   close db
